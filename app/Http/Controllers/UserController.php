bdf809f5b4171a5547b0cf469e45b0c73b1adaf5
Andy Nugraha Putra <andybima6@gmail.com>
2024-03-05 11:23:47 +0700

    Praktikum 2.2 - Jobsheet 4

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -69,11 +69,16 @@ class UserController extends Controller
         // });
         // return view('user', ['data' => $user]);
 
-        $user = UserModel::findOr(20,['username','nama'], function () {
-            abort(404);
-        });
-        return view('user', ['data' => $user]);
+        // $user = UserModel::findOr(20,['username','nama'], function () {
+        //     abort(404);
+        // });
+        // return view('user', ['data' => $user]);
 
+        //     $user = UserModel::findOrFail(1);
+        //     return view('user', ['data' => $user]);
 
+        //     $user = UserModel::where('username', 'manager9')->firstOrFail();
+        //     return view('user', ['data' => $user]);
+        // }
     }
 }
