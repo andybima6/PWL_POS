ab751981c957b20db35e8073f249d54b714822f4
Andy Nugraha Putra <andybima6@gmail.com>
2024-05-18 12:48:31 +0700

    JOBSHEET 12 FILE UPLOAD - ( B. . INFORMASI FILE UPLOAD  )

--- app/Http/Controllers/FileUploadController.php
+++ app/Http/Controllers/FileUploadController.php
@@ -6,10 +6,29 @@ use Illuminate\Http\Request;
 
 class FileUploadController extends Controller
 {
-    public function fileUpload(){
+    public function fileUpload()
+    {
         return view('file-upload');
     }
-    public function prosesFileUpload(Request $request){
-        return "Pemrosesan file upload disini";
+    public function prosesFileUpload(Request $request)
+    {
+        // dump($request->berkas);
+        // return "Pemrosesan file upload disini";
+
+        if ($request->hasFile('berkas')) {
+            echo "path(): " . $request->berkas->extension();
+            echo "<br>";
+            echo "extension(): " . $request->berkas->extension();
+            echo "<br>";
+            echo "getClientOriginalExtension(): " . $request->berkas->getClientOriginalExtension();
+            echo "<br>";
+            echo "getMimeType(): " . $request->berkas->getMimeType();
+            echo "<br>";
+            echo "getClientOriginalName(): " . $request->berkas->getClientOriginalName();
+            echo "<br>";
+            echo "getSize(): " . $request->berkas->getSize();
+        } else {
+            echo "Tidak ada berkas yang diupload";
+        }
     }
 }
--- resources/views/file-upload.blade.php
+++ resources/views/file-upload.blade.php
@@ -9,6 +9,19 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 </head>
+<style>
+    body,
+    html {
+        margin: 0;
+        padding: 0;
+        height: 100%;
+        background: url('image/cat.jpg');
+        background-size: cover;
+    }
+    h1,h2,label{
+        color: white;
+    }
+</style>
 
 <body>
     <div class="container mt-3">
